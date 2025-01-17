2caf596a1a2f8afbc5c3539c3833ba32756c2bca
antonio-dev2710 <antonio.cunha9610@gmail.com>
2025-01-17 19:55:39 -0300

    integration front and back

--- back-end-doc-ia/main.py
+++ back-end-doc-ia/main.py
@@ -2,12 +2,14 @@
 
 # Configurações do Flask
 import os
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask.cli import load_dotenv
+from flask_cors import CORS
 import openai
 
 
 app = Flask(__name__)
+CORS(app)
 app.config['UPLOAD_FOLDER'] = 'uploads/'
 os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
 
@@ -15,14 +17,16 @@ os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
 load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-@app.route('/test-openai')
+@app.route('/test-openai', methods=['POST'])
 def test_openai():
     try:
+        data = request.json
+        user_input = data.get('text', '')
         response = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
             messages=[
                 {"role": "system", "content": "Você é um professsor de matemática."},
-                {"role": "user", "content": "me fale quanto é 2+2"},
+                {"role": "user", "content": user_input},
             ]
         )
        
